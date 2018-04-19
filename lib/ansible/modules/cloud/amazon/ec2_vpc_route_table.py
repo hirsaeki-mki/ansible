4ed8d430d44cbe1466c31b337ecfc60abeadfcf4
Hiroshi Saeki <saeki-hiroshi@mki.co.jp>
2018-04-19 21:03:52 +0900

    ignore route for s3 endpoint 2

--- lib/ansible/modules/cloud/amazon/ec2_vpc_route_table.py
+++ lib/ansible/modules/cloud/amazon/ec2_vpc_route_table.py
@@ -610,6 +610,8 @@ def create_route_spec(connection, module, vpc_id):
             route_spec['gateway_id'] = igw
         if route_spec.get('gateway_id') and route_spec['gateway_id'].startswith('nat-'):
             rename_key(route_spec, 'gateway_id', 'nat_gateway_id')
+        if route_spec.get('interface_id'):
+            rename_key(route_spec, 'interface_id', 'network_interface_id')
 
     return snake_dict_to_camel_dict(routes, capitalize_first=True)
 
